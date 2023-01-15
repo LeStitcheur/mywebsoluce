57f593c7989bfe225beb5c0d9ee784699ca6c766
LeStitcheurF0u <mcfly@mywebsoluce.fr>
2023-01-15 16:19:38 +0100

    update progress bar

---
+++ app.js
@@ -0,0 +1,26 @@
+const progessBar = document.querySelector('.scrollbar');
+const main = document.querySelector('main');
+
+let totalHeight = main.scrollHeight - document.documentElement.clientHeight;
+
+console.log(progessBar)
+console.log(totalHeight)
+
+window.addEventListener('scroll', () => {
+
+    let progress  = (document.documentElement.scrollTop / totalHeight) * 100;
+    
+    progessBar.style.width = `${progress}%`;
+})
+
+const progessBarClick = document.querySelector('.clickScrollbar');
+
+progessBarClick.addEventListener('click', (e) => {
+
+    let newPageScroll = e.clientX / progessBarClick.offsetWidth * totalHeight;
+    window.scrollTo({
+        top: newPageScroll,
+        behavior: 'smooth'
+    })
+
+})
