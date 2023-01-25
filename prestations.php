11e17a1bf83c9ce42cb09f99d9191a76271b3100
LeStitcheurF0u <mcfly@mywebsoluce.fr>
2023-01-25 15:35:32 +0100

    import header.html && change link location for social

--- prestations.php
+++ prestations.php
@@ -8,72 +8,14 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/prestations.css">
     <link rel="shortcut icon" href="assets/img/Logo-Blue.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="../css/social.css">
 </head>
 
 <body>
-    <header>
-        <div class="logo">
-            <a href="index.php" class='desk'>
-                <img src="assets/img/MyWebSoluce-Logo-Ligne-Blue.png" alt="Logo MyWebSoluce">
-            </a>
-            <a href="index.php" class='mobile'>
-                <img src="assets/img/MyWebSoluce-Logo-White.png" alt="Logo MyWebSoluce">
-            </a>
-        </div>
-        <nav id="menu">
-            <ul class="menu__desktop">
-                <li>
-                    <a href="index.php">Accueil</a>
-                </li>
-                <li>
-                    <a href="about.php">Qui suis-je ?</a>
-                </li>
-                <li>
-                    <a href="prestations.php" class='active'>Prestations</a>
-                </li>
-                <li>
-                    <a href="outils.php">Boîte à outils</a>
-                </li>
-                <li>
-                    <a href="real.php">Réalisations</a>
-                </li>
-                <li>
-                    <a href="contact.php">Contact</a>
-                </li>
-            </ul>
-            <div class="menu__mobile">
-                <span class="menu__mobile__trigger">
-                    <svg viewBox="0 0 175 80" width="40" height="40">
-                        <rect width="80" height="15" fill="#f0f0f0" rx="10"></rect>
-                        <rect y="30" width="80" height="15" fill="#f0f0f0" rx="10"></rect>
-                        <rect y="60" width="80" height="15" fill="#f0f0f0" rx="10"></rect>
-                    </svg>
-                    <span>MENU</span>
-                </span>
-                <ul class="menu__mobile__list">
-                    <li>
-                        <a href="index.php">Accueil</a>
-                    </li>
-                    <li>
-                        <a href="about.php">Qui suis-je ?</a>
-                    </li>
-                    <li>
-                        <a href="prestations.php" class='active'>Prestations</a>
-                    </li>
-                    <li>
-                        <a href="outils.php">Boîte à outils</a>
-                    </li>
-                    <li>
-                        <a href="real.php">Réalisations</a>
-                    </li>
-                    <li>
-                        <a href="contact.php">Contact</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-
-    </header>
+    <?php
+    include 'import/header.html';
+    ?>
     <main>
         <?php
         include 'import/social.html';
